30142d03bc53fa2ed544b27b214d4fd5da8d3298
Cedrick Cooke <development@cedrickc.net>
2025-11-03 16:13:06 -0800

    CMP-8068 Provide default density on headless desktop

--- components/resources/library/src/desktopMain/kotlin/org/jetbrains/compose/resources/ResourceEnvironment.desktop.kt
+++ components/resources/library/src/desktopMain/kotlin/org/jetbrains/compose/resources/ResourceEnvironment.desktop.kt
@@ -2,6 +2,7 @@ package org.jetbrains.compose.resources
 
 import org.jetbrains.skiko.SystemTheme
 import org.jetbrains.skiko.currentSystemTheme
+import java.awt.HeadlessException
 import java.awt.Toolkit
 import java.util.*
 
@@ -9,7 +10,12 @@ internal actual fun getSystemEnvironment(): ResourceEnvironment {
     val locale = Locale.getDefault()
     //FIXME: don't use skiko internals
     val isDarkTheme = currentSystemTheme == SystemTheme.DARK
-    val dpi = Toolkit.getDefaultToolkit().screenResolution
+    val dpi = try {
+        Toolkit.getDefaultToolkit().screenResolution
+    } catch (_: HeadlessException) {
+        // Default to 1x ("unscaled") resources when DPI info not available 
+        DensityQualifier.MDPI.dpi
+    }
     return ResourceEnvironment(
         language = LanguageQualifier(locale.language),
         region = RegionQualifier(locale.country),
